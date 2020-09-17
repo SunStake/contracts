a9c5bbd2b28be02ab08641a00b9e1f29dc7dc92a
SunStake <sunstake@protonmail.com>
2020-09-17 16:13:08 +0000

    Add minting lock to reduce trust needed

--- contracts/SskToken.sol
+++ contracts/SskToken.sol
@@ -10,6 +10,12 @@ import "./tokens/TRC20Detailed.sol";
  * governance contract once the platform is considered mature. The team will
  * only use the minting functionality to distribute initial tokens.
  *
+ * This contract also features the capability of locking up minting. which
+ * rejects all minting requests until a specified timestamp that can be further
+ * extended if necessary. The team will lock up the minting functionality once
+ * it finishs the initial token distribution so that users won't have to trust
+ * the team to not mint additional tokens.
+ *
  * Idealy only peer-reviewed smart contracts should be added as minters.
  * Adding an external owned accounts (EOA) as minter is dangerous.
  */
@@ -18,10 +24,17 @@ contract SskToken is
     TRC20Detailed("SunStake", "SSK", 18),
     Ownable
 {
+    uint256 public mintingUnlockTime;
+
     mapping(address => bool) _minters;
 
     modifier onlyMinter() {
         require(isMinter(msg.sender), "SSK: caller is not a minter");
+        _;
+    }
+
+    modifier mintingNotLocked() {
+        require(!isMintingLocked(), "SSK: minting is locked");
         _;
     }
 
@@ -32,6 +45,14 @@ contract SskToken is
     function isMinter(address account) public view returns (bool) {
         require(account != address(0), "SSK: account is the zero address");
         return _minters[account];
+    }
+
+    /**
+     * @dev Returns if minting is locked up now
+     * @return bool
+     */
+    function isMintingLocked() public view returns (bool) {
+        return mintingUnlockTime > 0 && block.timestamp < mintingUnlockTime;
     }
 
     /**
@@ -51,6 +72,17 @@ contract SskToken is
     }
 
     /**
+     * @dev Extends the minting lock-up period.
+     */
+    function setMintingUnlockTime(uint256 unlockTime) public onlyOwner {
+        require(
+            unlockTime > mintingUnlockTime,
+            "SSK: only extension is allowed"
+        );
+        mintingUnlockTime = unlockTime;
+    }
+
+    /**
      * @dev Creates `amount` tokens and assigns them to `account`, increasing
      * the total supply.
      * @return bool
@@ -58,6 +90,7 @@ contract SskToken is
     function mint(address account, uint256 amount)
         public
         onlyMinter
+        mintingNotLocked
         returns (bool)
     {
         _mint(account, amount);
