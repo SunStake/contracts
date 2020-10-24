0543c94b33cf231c10d1390a9322591e7ea529a3
SunStake <sunstake@protonmail.com>
2020-10-24 09:21:44 +0000

    Add basic ExchangeRates test cases

---
+++ tests/ExchangeRates.spec.ts
@@ -0,0 +1,89 @@
+import { expect, use } from "chai";
+import { ethers, Contract, Wallet } from "ethers";
+import { deployContract, MockProvider, solidity } from "ethereum-waffle";
+
+import { expandTo18Decimals } from "./utilities";
+
+import ExchangeRates from "../build/ExchangeRates.json";
+
+use(solidity);
+
+describe("Airdrop", () => {
+  let provider: MockProvider;
+
+  let developer: Wallet;
+  let alice: Wallet;
+  let bob: Wallet;
+  let charlie: Wallet;
+  let david: Wallet;
+
+  let exchangeRates: Contract;
+
+  beforeEach(async () => {
+    provider = new MockProvider({
+      ganacheOptions: {
+        time: new Date(2020, 0, 1),
+      },
+    });
+    [developer, alice, bob, charlie, david] = provider.getWallets();
+
+    // Alice is oracle
+    exchangeRates = await deployContract(developer, ExchangeRates, [
+      alice.address,
+    ]);
+  });
+
+  it("only oracle can set rates", async () => {
+    await expect(
+      exchangeRates
+        .connect(bob)
+        .updateRates(
+          [ethers.utils.formatBytes32String("sBTC")],
+          [expandTo18Decimals(10000)],
+          Math.floor(new Date(2020, 0, 1).getTime() / 1000)
+        )
+    ).to.revertedWith("ExchangeRates: not oracle");
+
+    // Even owner is not allowed
+    await expect(
+      exchangeRates
+        .connect(developer)
+        .updateRates(
+          [ethers.utils.formatBytes32String("sBTC")],
+          [expandTo18Decimals(10000)],
+          Math.floor(new Date(2020, 0, 1).getTime() / 1000)
+        )
+    ).to.revertedWith("ExchangeRates: not oracle");
+
+    // Only oracle can call
+    await exchangeRates
+      .connect(alice)
+      .updateRates(
+        [ethers.utils.formatBytes32String("sBTC")],
+        [expandTo18Decimals(10000)],
+        Math.floor(new Date(2020, 0, 1).getTime() / 1000)
+      );
+  });
+
+  it("constant sUSD rate", async () => {
+    // Rate is already set to 1 without any oracle tx
+    expect(
+      (
+        await exchangeRates.getRateAndTime(
+          ethers.utils.formatBytes32String("sUSD")
+        )
+      ).rate
+    ).to.equal(expandTo18Decimals(1));
+
+    // Oracle cannot update sUSD rate
+    await expect(
+      exchangeRates
+        .connect(alice)
+        .updateRates(
+          [ethers.utils.formatBytes32String("sUSD")],
+          [expandTo18Decimals(10000)],
+          Math.floor(new Date(2020, 0, 1).getTime() / 1000)
+        )
+    ).to.revertedWith("ExchangeRates: cannot set sUSD rate");
+  });
+});
