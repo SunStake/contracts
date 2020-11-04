2e678e930c9282afe0788fb683d7395b3b323353
SunStake <sunstake@protonmail.com>
2020-11-04 18:07:30 +0000

    Fix typo

--- contracts/interfaces/IIssuableSynth.sol
+++ contracts/interfaces/IIssuableSynth.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.0;
 
 interface IIssuableSynth {
-    function issuer(address recipient, uint256 amount) external;
+    function issue(address recipient, uint256 amount) external;
 }
