ff1c0e0cc8af6ffc1aa356fc9506e055be096f74
SunStake <sunstake@protonmail.com>
2020-11-05 12:10:03 +0000

    Make Synth burnable

--- contracts/Synth.sol
+++ contracts/Synth.sol
@@ -1,13 +1,14 @@
 pragma solidity =0.5.12;
 
 import "./ownership/Ownable.sol";
+import "./tokens/TRC20Burnable.sol";
 import "./tokens/TRC20Detailed.sol";
 
 /**
  * @dev This contract represents a synthetic asset and implements the TRC20
  * standard, with a hard-coded fixed precision of 18 decimals.
  */
-contract Synth is Ownable, TRC20Detailed {
+contract Synth is Ownable, TRC20Burnable, TRC20Detailed {
     constructor(string memory name, string memory symbol)
         public
         TRC20Detailed(name, symbol, 18)
